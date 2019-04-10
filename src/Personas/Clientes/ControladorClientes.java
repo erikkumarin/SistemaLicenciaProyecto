9e40f25b1f43a06ac5f02cf33bf74f65aa2446e0
lcmurillor <42395232+lcmurillor@users.noreply.github.com>
2019-04-10 11:54:30 -0600

    Sentancias SQL

--- src/Personas/Clientes/ControladorClientes.java
+++ src/Personas/Clientes/ControladorClientes.java
@@ -8,17 +8,31 @@ public class ControladorClientes {
     
     
     
-    public void eliminar(){
-        BaseDatos BD = new BaseDatos();
+    public void eliminar(String cedula){
+        BaseDatos BD = new BaseDatos("DELETE FROM clientes WHERE Cedula ="+cedula);
     }
-    public void agregar(){
+    public void agregar(String cedula, String nombre, String fecha, String telefono, String correo){
+         BaseDatos BD = new BaseDatos("INSERT INTO clientes VALUES (?,?,?,?,?)");
+         BD.ejecutar(new Object[]{nombre,fecha,telefono,correo});
+    }
+    public void leer(String cedula){
+        BaseDatos BD = new BaseDatos("SELECT * FROM Clientes WHERE Cedula ="+cedula);
+    }
+    public void modificarCedula(){
         
     }
-    public void leer(){
+    public void modificarNombre(){
         
     }
-    public void modificar(){
+    public void modificarFecha(){
         
     }
+    public void modificarTelefono(){
+        
+    }
+    public void modificarCorreo(){
+        
+    }
+    
 
 }
