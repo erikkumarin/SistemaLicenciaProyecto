f8126665294cbb279c84eda60768b60d231a15c8
lcmurillor <42395232+lcmurillor@users.noreply.github.com>
2019-04-10 11:30:00 -0600

    Merge origin/master into HEAD

--- src/Personas/Clientes/ControladorClientes.java
+++ src/Personas/Clientes/ControladorClientes.java
@@ -1,4 +1,24 @@
 package Personas.Clientes;
+
+import BaseDeDatos.BaseDatos;
+
 public class ControladorClientes {
+    
+    private ModeloClientes cliente;
+    
+    
+    
+    public void eliminar(){
+        BaseDatos BD = new BaseDatos();
+    }
+    public void agregar(){
+        
+    }
+    public void leer(){
+        
+    }
+    public void modificar(){
+        
+    }
 
 }
