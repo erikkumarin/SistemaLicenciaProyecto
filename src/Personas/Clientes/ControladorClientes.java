5eeb1c522f27cc82dfac0756017404c63d6e49f9
lcmurillor <42395232+lcmurillor@users.noreply.github.com>
2019-04-10 14:21:09 -0600

    Merge origin/master into Luis
    Conflicts:
    	.gitignore
    	src/Personas/Clientes/ModeloClientes.java
    	src/Personas/clsPersonas.java

--- src/Personas/Clientes/ControladorClientes.java
+++ src/Personas/Clientes/ControladorClientes.java
@@ -3,26 +3,50 @@ package Personas.Clientes;
 import BaseDeDatos.BaseDatos;
 
 public class ControladorClientes {
-    
+
     private ModeloClientes cliente;
-     
-    public void eliminar(String cedula){
-        BaseDatos BD = new BaseDatos("DELETE FROM clientes WHERE Cedula ="+cedula);
-    }
-    public void agregar(String cedula, String nombre, String fecha, String telefono, String correo){
-         BaseDatos BD = new BaseDatos("INSERT INTO tblclientes VALUES (?,?,?,?,?)");
-         BD.ejecutar(new Object[]{cedula,nombre,fecha,telefono,correo});
-    }
-    public void leer(String cedula){
-        BaseDatos BD = new BaseDatos("SELECT * FROM tblclientes WHERE Cedula ="+cedula);
+
+    public void eliminar(String cedula) {
+        BaseDatos BD = new BaseDatos("DELETE FROM clientes WHERE Cedula =" + cedula);
     }
 
-    public void modificarTelefono(String valor, String telefono){
-        BaseDatos BD = new BaseDatos("UPDATE tblclientes SET Telefono ="+valor+ " WHERE Telefono="+ telefono);
+    public void agregar(String cedula, String nombre, String fecha, String telefono, String correo) {
+        BaseDatos BD = new BaseDatos("INSERT INTO tblclientes VALUES (?,?,?,?,?)");
+        BD.ejecutar(new Object[]{cedula, nombre, fecha, telefono, correo});
     }
-    public void modificarCorreo(String valor, String correo){
-        BaseDatos BD = new BaseDatos("UPDATE tblclientes SET Correo ="+valor+ " WHERE Correo="+ correo);
+
+    public void leer(String cedula) {
+        BaseDatos BD = new BaseDatos("SELECT * FROM tblclientes WHERE Cedula =" + cedula);
     }
-    
+
+    public void modificarTelefono(String telefono, String cedula) {
+        BaseDatos BD = new BaseDatos("UPDATE tblclientes SET Telefono =" + telefono + " WHERE Cedula =" + cedula);
+    }
+
+    public void modificarCorreo(String correo, String cedula) {
+        BaseDatos BD = new BaseDatos("UPDATE tblclientes SET Correo =" + correo + " WHERE Cedula =" + cedula);
+    }
+
+    private boolean verificarNombre(String nombre) {
+        return !nombre.trim().equals("");
+    }
+
+    private boolean verificarCedula(String cedula) {
+        try {
+            long id = Long.parseLong((cedula.replaceAll("-", "0")));
+            return id == 11;
+        } catch (NumberFormatException e) {
+        }
+        return false;
+    }
+
+    private boolean verificarTelefono(String telefono) {
+        try {
+            long numero = Long.parseLong(telefono);
+            return true;
+        } catch (NumberFormatException e) {
+        }
+        return false;
+    }
 
 }
