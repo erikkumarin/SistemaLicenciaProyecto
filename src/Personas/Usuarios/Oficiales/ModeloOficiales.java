a6f384eacfcdb509594c185daaafe86734f90d43
Fausto Rojas <faustorh@gmail.com>
2019-04-11 17:22:25 -0600

    modelado de personas hasta ahora

--- src/Personas/Usuarios/Oficiales/ModeloOficiales.java
+++ src/Personas/Usuarios/Oficiales/ModeloOficiales.java
@@ -6,27 +6,44 @@ public class ModeloOficiales extends ModeloUsuarios {
 
     private double salario;
 
-    public void calcularDeducEnfMat() {
-
+    public double getSalario() {
+        return salario;
     }
 
-    public void calcularDeducInvaMuer() {
-
+    public void setSalario(double salario) {
+        if (salario > 0) {
+            this.salario = salario;
+        }
     }
 
-    public void calcularDeducAporTrab() {
-
+    public double calcularDeducEnfMat() {
+        return salario * 0.055;
     }
 
-    public void calcularDeducAsocSoli() {
-
+    public double calcularDeducInvaMuer() {
+        return salario * 0.0384;
     }
 
-    public void calcularDeducImpRenta() {
-
+    public double calcularDeducAporTrab() {
+        return salario * 0.01;
     }
 
-    public void calcularsalarioNeto() {
-
+    public double calcularDeducAsocSoli() {
+        return salario * 0.033;
     }
+
+    public double calcularDeducImpRenta() {
+        if (salario > 817000 && salario <= 1226000) {
+            return salario * 0.1;
+        } else if (salario > 1226000) {
+            return salario * 0.15;
+        } else {
+            return salario;
+        }
+    }
+
+    public double calcularsalarioNeto() {
+        return salario -= (calcularDeducInvaMuer() + calcularDeducImpRenta() + calcularDeducEnfMat() + calcularDeducAsocSoli() + calcularDeducAporTrab());
+    }
+
 }
--- src/Personas/clsPersonas.java
+++ src/Personas/clsPersonas.java
@@ -1,25 +1,45 @@
 package Personas;
 
 public abstract class clsPersonas {
+
     private String cedula;
     private String nombre;
     private String telefono;
     private String correo;
 
-
     public void leer() {
-
     }
 
-    public void eliminar() {
-
+    public String getCedula() {
+        return cedula;
     }
 
-    public void actualizar() {
-
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
     }
 
-    public void crear() {
-
+    public String getNombre() {
+        return nombre;
     }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
 }
