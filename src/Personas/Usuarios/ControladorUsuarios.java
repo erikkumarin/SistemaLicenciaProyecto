4a900f93418be07e8a326979ef49e9dc0bf4309f
Erick Marín <erikkumarin@users.noreply.github.com>
2019-04-12 17:27:46 -0500

    Error Conexion

--- src/Personas/Usuarios/ControladorUsuarios.java
+++ src/Personas/Usuarios/ControladorUsuarios.java
@@ -1,36 +1,37 @@
 package Personas.Usuarios;
 
 import BaseDeDatos.BaseDatos;
+import Errores.ErrorConexion;
 
 public class ControladorUsuarios {
 
     private ModeloUsuarios usuario;
     private BaseDatos BD;
 
-    public void eliminar(String cedula) {
+    public void eliminar(String cedula) throws ErrorConexion {
         if (verificarCedula(cedula)) {
             BD = new BaseDatos("DELETE FROM tblusuarios WHERE Cedula =" + cedula);
         }
     }
 
-    public void agregar(String cedula, String nombre, String fecha, String telefono, String correo, String tipo, String salario) {
+    public void agregar(String cedula, String nombre, String fecha, String telefono, String correo, String tipo, String salario) throws ErrorConexion {
         BD = new BaseDatos("INSERT INTO tblusuarios VALUES (?,?,?,?,?,?,?)");
         BD.ejecutar(new Object[]{cedula, nombre, fecha, telefono, correo, tipo, salario});
     }
 
-    public void leer(String cedula) {
+    public void leer(String cedula) throws ErrorConexion {
         if (verificarCedula(cedula)) {
             BD = new BaseDatos("SELECT * FROM tblusuarios WHERE Cedula =" + cedula);
         }
     }
 
-    public void modificarTelefono(String telefono, String cedula) {
+    public void modificarTelefono(String telefono, String cedula) throws ErrorConexion {
         if (verificarCedula(cedula)) {
             BD = new BaseDatos("UPDATE tblusuarios SET Telefono =" + telefono + " WHERE Cedula =" + cedula);
         }
     }
 
-    public void modificarCorreo(String correo, String cedula) {
+    public void modificarCorreo(String correo, String cedula) throws ErrorConexion {
         if (verificarCedula(cedula)) {
             BD = new BaseDatos("UPDATE tblusuarios SET Correo =" + correo + " WHERE Cedula =" + cedula);
         }
