96d2b1b6125a5b1d85d244b2d05d009b8cf95714
lcmurillor <42395232+lcmurillor@users.noreply.github.com>
2019-04-22 14:29:05 -0600

    Merge origin/Hamklien into Luis
    Conflicts:
    	Config.ini
    	src/Citas/VistaCitas.form
    	src/Citas/VistaCitas.java

--- src/Utilidades/ValidarCorreo.java
+++ src/Utilidades/ValidarCorreo.java
@@ -5,8 +5,7 @@ import java.util.regex.Pattern;
 
 public class ValidarCorreo {
 
-    //"[_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@(\\.[a-z0-9]+)(\\.[a-z]{2,4})*(\\.[a-z]{2,3})*"
-    private static String correo = "[_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-z]+(\\.[a-z]{2,4})*(\\.[a-z]{2,3})*";
+    private static String correo = "[_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-z]+(\\.[a-z]{2,4})*";
     private static Pattern patron;
     private static Matcher comparar;
     
