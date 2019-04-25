064769c943be88fe3655ebe26be5f8b809c1a6a7
lcmurillor <42395232+lcmurillor@users.noreply.github.com>
2019-04-24 22:31:17 -0600

    Merge origin/Erick into Luis
    Conflicts:
    	Config.ini
    	src/Personas/Clientes/ModeloClientes.java

--- src/Test/Main.java
+++
@@ -1,8 +0,0 @@
-package Test;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println(Utilidades.ValidarCorreo.Validar("erickmarin_16-2011@hotmail.com"));
-    }
-}
