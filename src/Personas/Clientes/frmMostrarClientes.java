617eaa345840e1a55d8926795eca048715820519
Hamklien <hamklin98@gmail.com>
2019-04-25 18:52:59 -0600

    Merge origin/Erick into Hamklien
    Conflicts:
    	Config.ini
    	src/Citas/ControladorCitas.java
    	src/Citas/clsCitas.java
    	src/Citas/frmMostrarCitas.form
    	src/Citas/frmMostrarCitas.java
    	src/Main/frmPrincipal.form
    	src/Main/frmPrincipal.java
    	src/Personas/Usuarios/ControladorUsuarios.java
    	src/Personas/Usuarios/frmIniciarSesion.java
    	src/Pruebas/clsPruebas.java
    	src/Pruebas/frmPruebas.form
    	src/Pruebas/frmPruebas.java
    	src/Utilidades/Fecha.java

--- src/Personas/Clientes/frmMostrarClientes.java
+++ src/Personas/Clientes/frmMostrarClientes.java
@@ -1,5 +1,11 @@
-package Citas;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Personas.Clientes;
 
+import Citas.clsCitas;
 import Errores.ErrorConexion;
 import Main.frmPrincipal;
 import Personas.Usuarios.frmIniciarSesion;
@@ -10,25 +16,26 @@ import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-public class frmMostrarCitas extends javax.swing.JInternalFrame {
+/**
+ *
+ * @author Universidad
+ */
+public class frmMostrarClientes extends javax.swing.JInternalFrame {
 
-    private ControladorCitas citas;
+    private ControladorClientes clientes;
     private DefaultTableModel tbl;
-    int cont = 0;
 
-    public frmMostrarCitas() {
+    /**
+     * Creates new form frmMostrarClientes
+     */
+    public frmMostrarClientes() {
         initComponents();
-        tbl = (DefaultTableModel) tblCitas.getModel();
-        citas = new ControladorCitas();
-        cargarTabla();
-    }
-
-    private void cargarTabla() {
+        tbl = (DefaultTableModel) tblClientes.getModel();
+        clientes = new ControladorClientes();
         try {
-            System.out.println("entro: " + ++cont);
-            citas.cargarTabla(this);
+            clientes.cargarUsuarios(this);
         } catch (ErrorConexion ex) {
-            Logger.getLogger(frmMostrarCitas.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(frmMostrarClientes.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
@@ -42,23 +49,23 @@ public class frmMostrarCitas extends javax.swing.JInternalFrame {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        tblCitas = new javax.swing.JTable();
+        tblClientes = new javax.swing.JTable();
 
         setClosable(true);
 
-        tblCitas.setModel(new javax.swing.table.DefaultTableModel(
+        tblClientes.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
             new String [] {
-                "Id", "Cedula", "Fecha", "Hora"
+                "Cedula", "Nombre", "Edad", "Telefono", "Correo"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+                java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, false, false
+                false, false, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -69,12 +76,12 @@ public class frmMostrarCitas extends javax.swing.JInternalFrame {
                 return canEdit [columnIndex];
             }
         });
-        tblCitas.addMouseListener(new java.awt.event.MouseAdapter() {
+        tblClientes.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                tblCitasMouseClicked(evt);
+                tblClientesMouseClicked(evt);
             }
         });
-        jScrollPane1.setViewportView(tblCitas);
+        jScrollPane1.setViewportView(tblClientes);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -82,54 +89,45 @@ public class frmMostrarCitas extends javax.swing.JInternalFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 545, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 664, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 514, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 456, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void tblCitasMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblCitasMouseClicked
-        int i = tblCitas.getSelectedRow();
+    private void tblClientesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblClientesMouseClicked
+        int i = tblClientes.getSelectedRow();
         if (i != -1) {
             try {
-                int opc = JOptionPane.showConfirmDialog(this, "¿Esta seguro que desea aplicar esta Cita?", "Confirmación", 0, 2);
+                int opc = JOptionPane.showConfirmDialog(this, "¿Esta seguro que desea seleccionar este usuario?", "Confirmación", 0, 2);
                 if (opc == 0) {
-                    clsCitas cita = citas.pasarDatos(this, i);
-                    frmPruebas prueba = new frmPruebas();
-                    prueba.setNombre(cita.getCliente().getNombre());
-                    prueba.setCedula(cita.getCliente().getCedula());
-                    prueba.setEdad(String.valueOf(cita.getCliente().calcularEdad()));
-                    prueba.setFecha(cita.getFecha());
-                    prueba.setHora(cita.getHora());
-                    prueba.setCedulaOficial(frmIniciarSesion.getCedulaOficial());
-                    prueba.setOficial(frmIniciarSesion.getNombreOficial());
-                    frmPrincipal.agregar(prueba);
+                  clientes.pasarClientes(this,i);
                 }
             } catch (ErrorConexion ex) {
-                JOptionPane.showMessageDialog(this, ex.getMessage(), "Error", 0);
             }
         }
-    }//GEN-LAST:event_tblCitasMouseClicked
+    }//GEN-LAST:event_tblClientesMouseClicked
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable tblCitas;
+    private javax.swing.JTable tblClientes;
     // End of variables declaration//GEN-END:variables
 
-    public JTable getTblCitas() {
-        return tblCitas;
+    public JTable getTblClientes() {
+        return tblClientes;
     }
 
-    public void setTblCitas(JTable tblCitas) {
-        this.tblCitas = tblCitas;
+    public void setTblClientes(JTable tblClientes) {
+        this.tblClientes = tblClientes;
     }
+
 }
