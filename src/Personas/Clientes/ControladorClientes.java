11b7ea4cdeac3e1bb373043e8a173a4963205e72
Fausto Rojas <faustorh@gmail.com>
2019-04-25 19:06:19 -0600

    Merge origin/Fausto into Erick
    Conflicts:
    	Config.ini
    	src/Personas/Usuarios/frmEditarUsuario.form
    	src/Personas/Usuarios/frmEditarUsuario.java

--- src/Personas/Clientes/ControladorClientes.java
+++ src/Personas/Clientes/ControladorClientes.java
@@ -4,6 +4,7 @@ import BaseDeDatos.BaseDatos;
 import Errores.ErrorConexion;
 import Errores.ErrorMensaje;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 public class ControladorClientes {
 
@@ -13,12 +14,12 @@ public class ControladorClientes {
     public ControladorClientes() {
     }
     
-    public void agregar(frmRegistrarCliente vista) throws ErrorConexion {
+   public void agregar(frmRegistrarCliente vista) throws ErrorConexion {
         BD = new BaseDatos("INSERT INTO tblclientes VALUES (?,?,?,?,?)");
         cliente = new clsClientes(vista.getCedula(), vista.getNombre(), vista.getFecha(), vista.getTelefono(), vista.getCorreo());
         if (ErrorMensaje.mostrarMensajes()) {
             JOptionPane.showMessageDialog(vista, ErrorMensaje.getMsj(), "Error", 0);
-        }else{
+        } else {
             BD.ejecutar(new Object[]{cliente.getCedula(), cliente.getNombre(), cliente.getFechaNac(), cliente.getTelefono(), cliente.getCorreo()});
             vista.dispose();
         }
@@ -30,7 +31,7 @@ public class ControladorClientes {
         BD = new BaseDatos("DELETE FROM tblclientes WHERE Cedula =" + cliente.getCedula());
         BD.ejecutar();
     }
-    
+
     public void leer(frmRegistrarCliente vista) throws ErrorConexion {
         cliente = new clsClientes();
         cliente.setCedula(vista.getCedula());
@@ -40,6 +41,29 @@ public class ControladorClientes {
 
     public void modificar() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void cargarUsuarios(frmMostrarClientes vista) throws ErrorConexion {
+        BD = new BaseDatos("Select * from tblClientes");
+        BD.ejecutar();
+        DefaultTableModel modelo = (DefaultTableModel) vista.getTblClientes().getModel();
+        modelo.setNumRows(0);
+        Object obj[];
+        do {
+            obj = BD.getObjet();
+            if (obj != null) {
+                cliente = new clsClientes(obj);
+                modelo.addRow(cliente.toObject());
+            }
+        } while (obj != null);
+    }
+    
+    public  clsClientes pasarClientes(frmMostrarClientes vista, int indice) throws ErrorConexion{
+        BD = new BaseDatos("SELECT * from tblClientes where Cedula=?");
+        BD.ejecutar(new Object[]{vista.getTblClientes().getValueAt(indice, 0).toString()});
+        Object obj[] = BD.getObjet();
+        cliente= new clsClientes(obj);
+        return cliente;
     }
 
 }
