88ea79033a48ea1a9f84fe09928fc7a9c6461349
Fausto Rojas <faustorh@gmail.com>
2019-04-25 20:21:36 -0600

    Merge origin/Luis into Fausto
    Conflicts:
    	Config.ini
    	src/Personas/Clientes/ControladorClientes.java
    	src/Personas/Clientes/clsClientes.java
    	src/Personas/Clientes/frmMostrarClientes.form
    	src/Personas/Clientes/frmMostrarClientes.java

--- src/Personas/Clientes/frmMostrarClientes.java
+++ src/Personas/Clientes/frmMostrarClientes.java
@@ -71,15 +71,15 @@ public class frmMostrarClientes extends javax.swing.JInternalFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jScrollPane1)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jScrollPane1)
+                .addGap(20, 20, 20))
         );
 
         pack();
