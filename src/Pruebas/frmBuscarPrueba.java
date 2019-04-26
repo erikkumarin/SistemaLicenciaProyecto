9205fd32f17f1320d0aa032a10d2328b2d98b2a1
lcmurillor <42395232+lcmurillor@users.noreply.github.com>
2019-04-26 14:20:16 -0600

    Merge origin/Erick into Luis
    Conflicts:
    	Config.ini

--- src/Pruebas/frmBuscarPrueba.java
+++ src/Pruebas/frmBuscarPrueba.java
@@ -67,9 +67,16 @@ public class frmBuscarPrueba extends javax.swing.JInternalFrame {
             Class[] types = new Class [] {
                 java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         jScrollPane1.setViewportView(tblPruebas);
